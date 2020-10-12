6ba52de28ebd31ab09ec5b14fab322a8a213ed0e
Alexander <lotos_toi@mail.ru>
2020-10-12 23:11:25 +1000

    add 4 components

--- frontend/my-project/src/App.vue
+++ frontend/my-project/src/App.vue
@@ -1,29 +1,36 @@
 <template>
-   <div class="wrapper">
-     <my-header/>
-
-   </div>
+  <div class="wrapper">
+    <my-header />
+    <my-nuv />
+    <baner />
+    <category />
+  </div>
 </template>
 
 <script>
-import MyHeader from "@/components/header" 
+import MyHeader from "@/components/header";
+import MyNuv from "@/components/nav";
+import Baner from "@/components/baner";
+import Category from "@/components/category";
 
 export default {
   //name: 'App',
   components: {
-     MyHeader
-  }
-}
+    MyHeader,
+    MyNuv,
+    Baner,
+    Category,
+  },
+};
 </script>
 
 <style lang="scss">
-
- .wrapper {
-    width: 100%;
-    margin: 0 auto;
-    font-family: "Lato", sans-serif;
-    font-size: 14px;
-    font-weight: 400;
-    color: $base;
+.wrapper {
+  width: 100%;
+  margin: 0 auto;
+  font-family: "Lato", sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  color: $base;
 }
 </style>
