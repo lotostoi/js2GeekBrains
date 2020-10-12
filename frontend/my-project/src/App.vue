b6c9516644ec664cffb303fe666e975c1f80ba82
lotostoii <lotostoii@gmail.com>
2020-10-12 17:17:51 +1000

    firts component

---
+++ frontend/my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="wrapper">
+     <my-header/>
+
+   </div>
+</template>
+
+<script>
+import MyHeader from "@/components/header" 
+
+export default {
+  //name: 'App',
+  components: {
+     MyHeader
+  }
+}
+</script>
+
+<style lang="scss">
+
+ .wrapper {
+    width: 100%;
+    margin: 0 auto;
+    font-family: "Lato", sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    color: $base;
+}
+</style>
---
+++ frontend/my-project/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/scss/my_var.scss";`
+      }
+    }
+  }
+}
