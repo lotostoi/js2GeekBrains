1b551fc29a7d80764333b56ae6300d4faa6e7434
Alexander Plotnikov <lotos_toi@mail.ru>
2020-11-29 13:40:18 +1000

    add search

--- src/js/ListGoods.js
+++ src/js/ListGoods.js
@@ -1,100 +1,91 @@
-import Good  from "./Good"
-import Cart  from "./Cart"
-import constants from "./constants"
-import getRequest from "./toPromis"
-
-
-
+import Good from './Good'
+import Cart from './Cart'
+import constants from './constants'
+import getRequest from './toPromis'
+import Seacrh from './Search'
 
 class ListGoods {
+  privat = { goodsForWork: [] }
+  constructor(container = '.products') {
+    this.container = document.querySelector(container)
+    this.#goods = []
+    this.allGoods = []
+    this.resolve
+    this.cart = new Cart()
+    this.search = null
+    this.ready()
+    this.#init()
+  }
 
-    constructor(container = '.products') {
+  ready() {
+    return new Promise((res) => {
+      this.resolve = res
+    })
+  }
 
-        this.container = document.querySelector(container)
+  async #init() {
+    await this.#fetchGoods()
+    this.search = new Seacrh('.search', this.#goods)
+    this.goodsForWork = this.search.catalogFilter
+    this.#rander()
+    this.#handler()
+  }
 
-        this._goods = []
+  get goodsForWork() {
+    return this.privat.goodsForWork
+  }
+  set goodsForWork(val) {
+    this.privat.goodsForWork = val
+    console.log('ttt')
+    this.#rander()
+    return true
+  }
 
-        this.allGoods = []
+  async #fetchGoods() {
+    let res = await getRequest(constants.catalog)
+    this.#goods = await JSON.parse(res)
+  }
 
-        this.resolve
-
-        this.cart = new Cart
-
-        this.ready()
-
-        this._init()
-
+  #rander() {
+    this.container.innerHTML = ''
+    for (let good of this.goodsForWork) {
+      const newGood = new Good(good)
+      this.allGoods.push(newGood)
+      this.container.insertAdjacentHTML('beforeend', newGood.rander())
     }
+    this.resolve()
+  }
 
-    ready() { return new Promise(res => { this.resolve = res }) }
+  #handler() {
+    let body = document.querySelector('body')
+    this.container.addEventListener('click', (e) => {
+      if (e.target.className === 'addToCart') {
+        let id = e.target.dataset.id
+        this.cart.addToCart(
+          this.allGoods.find((good) => good.id.toString() === id.toString())
+        )
+      }
 
-    async _init() {
+      if (e.target.className === 'dellFromCart') {
+        let id = e.target.dataset.id
+        this.cart.removeFromCart(
+          this.allGoods.find((good) => good.id.toString() === id.toString())
+        )
+      }
+    })
+    body.addEventListener('search', () => {
+      this.goodsForWork = this.search.catalogFilter
+    })
+  }
 
-        await this._fetchGoods()
-        this._rander()
-        this._handler()
+  // method for calculating of all sum
 
-
-    }
-
-    async _fetchGoods() {
-
-        let res = await getRequest(constants.catalog)
-        this._goods = await JSON.parse(res)
-
-
-    }
-
-
-    _rander() {
-
-        for (let good of this._goods) {
-
-            const newGood = new Good(good)
-            this.allGoods.push(newGood)
-            this.container.insertAdjacentHTML('beforeend', newGood.rander())
-
-        }
-        this.resolve()
-
-    }
-
-    _handler() {
-
-        this.container.addEventListener('click', (e) => {
-
-            if (e.target.className === 'addToCart') {
-
-                let id = e.target.dataset.id
-
-                this.cart.addToCart(this.allGoods.find(good => good.id.toString() === id.toString()))
-
-            }
-
-            if (e.target.className === 'dellFromCart') {
-
-                let id = e.target.dataset.id
-
-                this.cart.removeFromCart(this.allGoods.find(good => good.id.toString() === id.toString()))
-
-            }
-
-
-        })
-
-
-    }
-
-    // method for calculating of all sum 
-
-    async allSummAllGoods() {
-
-        await this.ready()
-
-        return this.allGoods.length > 0 ? this.allGoods.reduce((accum, good) => accum + good.price, 0) : 0
-
-    }
-
+  async allSummAllGoods() {
+    await this.ready()
+    return this.allGoods.length > 0
+      ? this.allGoods.reduce((accum, good) => accum + good.price, 0)
+      : 0
+  }
 }
 
 export default ListGoods
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,10 @@
 
 import "../css/style.scss"
-
+import '@babel/polyfill'
 import ListGoods from "./ListGoods"
-
-
-
-
-
 
 const cotalog = new ListGoods()
 
-
-/* const cartWrapper = document.querySelector('.cart')
-
-const closeCart = document.querySelector('.cart__close')
-
-const showCart = document.querySelector('.btn-cart')
-
-
-const hidenShowCart = (e) => {
-
-    cartWrapper.classList.toggle('cart-active')
-
-}
-
-closeCart.addEventListener('click', hidenShowCart)
-
-showCart.addEventListener('click', hidenShowCart)
- */
 
 
 
